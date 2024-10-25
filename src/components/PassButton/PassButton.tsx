f795dbcb12284efec5532c89d68bcdd2c72508fc
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-10-25 15:08:12 -0300

    feat: adiciona as funções para gerar a senha e copiar para área de transferência

--- src/components/PassButton/PassButton.tsx
+++ src/components/PassButton/PassButton.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Pressable, Text, TouchableOpacity, View } from 'react-native';
+import * as Clipboard from 'expo-clipboard';
 
 import { styles } from './styles';
 import { InputText } from '../TextInput/InputText';
+import generatePassword from '../../services/passwordService';
 
 export function PassButton() {
+
+  const [pass, setPass] = useState('')
+
+  function handleGeneretorButton(){
+    let passwordToken = generatePassword()
+    setPass(passwordToken)
+  }
+
+  function handleCopyButton(){
+    Clipboard.setStringAsync(pass);
+  }
+
   return (
     <>
-    <InputText />
+    <InputText pass={pass}/>
 
 
     <TouchableOpacity
-    onPress={() => {console.log("GENERATOR!")}}>
+    onPress={handleGeneretorButton}>
         <Text style={styles.buttonText}>GENERATOR</Text>
     </TouchableOpacity>
 
     <Pressable
-    onPress={() => {console.log("COPY!")}}>
+    onPress={handleCopyButton}>  
         <Text style={styles.buttonText}>📥  COPY</Text>
     </Pressable>
     </>    
