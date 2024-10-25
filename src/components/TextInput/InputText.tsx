ab1c134c9b79a4eb5f0dd5f94284108a543b8408
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-10-25 11:53:45 -0300

    feat: cria o componente de input p/ exibir a senha que sera gerada

---
+++ src/components/TextInput/InputText.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TextInput } from 'react-native';
+
+import { styles } from './styles';
+
+export function InputText() {
+  return (    
+      <TextInput style={styles.input}
+      placeholder='pass'>
+      </TextInput>    
+  );
+}
---
+++ src/components/TextInput/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {},
+
+  input:{
+    backgroundColor: '#FFE608',
+    width: '100%',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 20,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    textAlign: 'center'
+  }
+
+
+});
